d52290b510280396134869688e85d584e99831f9
EC2 Default User <95359918+wazuo@users.noreply.github.com>
2021-12-27 05:05:21 +0000

    [update]応用課題３ [fix]model名誤りのため修正開始

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,10 +1,15 @@
 class PostCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    comment.book_id = @book.id
+    if comment.save
+      redirect_to book_path(@book)
+    else
+      @post_comment = comment
+      @booknew = Book.new
+      render "books/show"
+    end
   end
 
   def destroy
