f7848d43756e86b3d643f54ed41b4d9297397a88
EC2 Default User <95359918+wazuo@users.noreply.github.com>
2021-12-29 02:36:20 +0000

    [update]応用課題4 user.indexにフォロー機能搭載

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,24 @@ class User < ApplicationRecord
   attachment :profile_image, destroy: false
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :followers,  class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
+
+  # ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+  # フォローの確認を行う
+  def following?(user)
+    following_user.include?(user)
+  end
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+  post 'follow/:id' => 'relationships#follow', as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
 end
