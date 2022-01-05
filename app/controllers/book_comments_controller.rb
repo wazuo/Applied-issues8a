183218714d3d7cf828973dc4b8a27936a9509da1
EC2 Default User <95359918+wazuo@users.noreply.github.com>
2022-01-05 09:28:18 +0000

    [Update]応用課題6 コメント機能非同期化完了

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,21 +1,21 @@
 class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = @book.id
-    if comment.save
-      redirect_to book_path(@book)
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    if @comment.save
+      render "index"
     else
-      @book_comment = comment
+      @book_comment = @comment
       @booknew = Book.new
-      render "books/show"
+      render "error"
     end
   end
 
   def destroy
     BookComment.find_by(id: params[:id]).destroy
-    redirect_to book_path(params[:book_id])
-
+    @book = Book.find(params[:book_id])
+    render "index"
   end
 
   private
